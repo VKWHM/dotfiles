e21800d2e39468aa54a805188da605b9a626d1d2
VKWHM <111775089+VKWHM@users.noreply.github.com>
2024-11-03 09:29:56 +0300

    Adding alias for lastlog

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -95,3 +95,8 @@ alias lsize='du -sh * | sort -rh'
 
 # Show command usage statistics
 alias hs='history | awk "{CMD[\$2]++; count++;} END {for (a in CMD) print CMD[a] \" \" CMD[a]/count*100 \"% \" a;}" | grep -vE "^[%.0-9 ]+./" | column -c3 -s " " -t | sort -nr | nl | head -20'
+
+# Show last logins
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  alias lastlog="lastlog | grep -v 'Never logged in'"
+fi
