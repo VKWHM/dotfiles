4813d238ce6e42b173b44d1899bc7a83a21ef667
VKWHM <111775089+VKWHM@users.noreply.github.com>
2024-11-11 16:56:10 +0000

    Adding Neovim Noice.nvim plugin for popup cmdline and messages

---
+++ nvim/lua/whoami/plugins/noice.lua
@@ -0,0 +1,63 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+
+	opts = {},
+	dependencies = {
+		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+		"MunifTanjim/nui.nvim",
+		-- OPTIONAL:
+		--   `nvim-notify` is only needed, if you want to use the notification view.
+		--   If not available, we use `mini` as the fallback
+		"rcarriga/nvim-notify",
+	},
+	config = function()
+		local noice = require("noice")
+		noice.setup({
+			presets = {
+				long_message_to_split = true,
+			},
+			views = {
+				cmdline_popup = {
+					position = {
+						row = 5,
+						col = "50%",
+					},
+					size = {
+						width = 60,
+						height = "auto",
+					},
+				},
+				popupmenu = {
+					relative = "editor",
+					position = {
+						row = 8,
+						col = "50%",
+					},
+					size = {
+						width = 60,
+						height = 10,
+					},
+					border = {
+						style = "rounded",
+						padding = { 0, 1 },
+					},
+					win_options = {
+						winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+					},
+				},
+			},
+		})
+		require("lualine").setup({
+			sections = {
+				lualine_x = {
+					{
+						noice.api.statusline.mode.get,
+						cond = noice.api.statusline.mode.has,
+						color = { fg = "#ff9e64" },
+					},
+				},
+			},
+		})
+	end,
+}
