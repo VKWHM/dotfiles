d7f62da059edfda4d2a96cd804f96f416f702b99
VKWHM <111775089+VKWHM@users.noreply.github.com>
2024-11-23 22:08:01 +0300

    Adding copilot toggle keymap

--- nvim/lua/whoami/plugins/copilot.lua
+++ nvim/lua/whoami/plugins/copilot.lua
@@ -1,3 +1,14 @@
 return {
 	"github/copilot.vim",
+	config = function()
+		local keymap = vim.keymap
+		keymap.set("n", "<leader>uc", function()
+			if vim.g.copilot_enabled == 1 then
+				vim.cmd([[Copilot disable]])
+			else
+				vim.cmd([[Copilot enable]])
+			end
+			vim.cmd([[Copilot status]])
+		end, { desc = "Toggle Copilot" })
+	end,
 }
