b7cec27c32fc425e95568cd79b211321511beae5
VKWHM <111775089+VKWHM@users.noreply.github.com>
2024-12-26 19:52:45 +0000

    Lazyvim adding plugins extras to lazyvim.json file

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -20,8 +20,6 @@ require("lazy").setup({
 		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
 		-- import/override with your plugins
 		{ import = "plugins" },
-		{ import = "lazyvim.plugins.extras.editor.aerial" },
-		{ import = "lazyvim.plugins.extras.ai.copilot-chat" },
 	},
 	defaults = {
 		-- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
