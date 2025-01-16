38066ec0059afff66f3c539f9a016502e8551b41
VKWHM <alasaal3@gmail.com>
2025-01-16 14:45:20 +0300

    Creating Autocmd and shortcut for plugin development
    Autocmd reload plugin when changed and P shortcut for printing table

--- nvim/lua/config/dev.lua
+++ nvim/lua/config/dev.lua
@@ -1,3 +1,25 @@
+-- Shortcuts
+P = function(tbl)
+	print(vim.inspect(tbl))
+end
+
+-- Keymaps
 local map = vim.keymap.set
 
-map("n", "<leader>vv", "<Plug>PlenaryTestFile", { desc = "Run all tests in file" })
+map("n", "<leader>vv", "<cmd>PlenaryBustedFile %<CR>", { desc = "Run all tests in file" })
+
+-- Autocmds
+
+local PluginDevelopemnt = vim.api.nvim_create_augroup("PluginDevelopemnt", {
+	clear = true,
+})
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+	group = PluginDevelopemnt,
+	pattern = "*.nvim/*",
+	callback = function(ev)
+		local plugin_name = (ev.match or ""):match("^/.*/(.-)%.nvim")
+		package.loaded[plugin_name] = nil
+		vim.notify(("%s.nvim package reloaded!"):format(plugin_name), vim.log.levels.INFO)
+	end,
+})
