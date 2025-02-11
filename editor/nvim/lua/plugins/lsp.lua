44e0c51c8fc5443e8131263f343c0567b189b54a
VKWHM <alasaal3@gmail.com>
2025-02-11 15:01:14 +0300

    Improve neovim editor

--- editor/nvim/lua/plugins/lsp.lua
+++ editor/nvim/lua/plugins/lsp.lua
@@ -27,24 +27,27 @@ return {
 			local vtsls_setup = opts.setup.vtsls
 			opts.setup.vtsls = function(_, opts)
 				local util = require("lspconfig.util")
-				if util.root_pattern("deno.json", "deno.jsonc")(opts.root_dir or vim.uv.cwd()) then
+				if
+					util.root_pattern("deno.json", "deno.jsonc")(opts.root_dir or vim.uv.cwd())
+					and not util.root_pattern("package.json", "package-lock.json")(opts.root_dir or vim.uv.cwd())
+				then
 					return true
 				end
 				return vtsls_setup(_, opts)
 			end
 			opts.setup.denols = function(_, deno_opts)
 				local util = require("lspconfig.util")
-				-- If we do NOT find a deno.json, disable denols
+				-- If we do NOT find a deno.{json,lock}, disable denols
 				if not util.root_pattern("deno.json", "deno.jsonc")(deno_opts.root_dir or vim.loop.cwd()) then
 					return true
 				end
 
 				-- Otherwise, merge the existing vtsls config into denols, so we don't
 				-- have to duplicate everything. Then override with actual deno settings.
-				local all_servers = require("lazyvim.util").opts("nvim-lspconfig").servers
-				if all_servers and all_servers.vtsls then
-					deno_opts = vim.tbl_deep_extend("force", deno_opts, all_servers.vtsls)
-				end
+				-- local all_servers = require("lazyvim.util").opts("nvim-lspconfig").servers
+				-- if all_servers and all_servers.vtsls then
+				-- 	deno_opts = vim.tbl_deep_extend("force", deno_opts, all_servers.vtsls)
+				-- end
 
 				-- Force certain deno settings or filetypes
 				deno_opts.settings = {
