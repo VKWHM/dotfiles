aeea1a2dfeb08849261fb77c7b18cd288d1c650d
VKWHM <111775089+VKWHM@users.noreply.github.com>
2025-04-14 13:13:51 +0000

    Starship shell themer added

--- nix/home.nix
+++ nix/home.nix
@@ -9,9 +9,19 @@ with user; {
   programs.zsh = import ./zsh.nix pkgs;
   programs.fzf = import ./fzf.nix  pkgs;
   programs.bat = import ./bat.nix pkgs;
+  programs.ripgrep = {
+    enable = true;
+    arguments = [
+      "--smart-case" 
+      "--max-columns-preview"
+    ];
+  };
+  programs.starship = {
+    enable = true;
+    settings = builtins.fromTOML (builtins.unsafeDiscardStringContext (builtins.readFile ../shell/starship.toml));
+  };
   programs.fd.enable = true;
   programs.git.enable = true;
-  programs.ripgrep.enable = true;
   programs.eza.enable = true;
   programs.zoxide.enable = true;
   programs.home-manager.enable = true;
