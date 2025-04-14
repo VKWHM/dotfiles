54cbaea8084d9c9080f8754e669454d94a1da593
VKWHM <111775089+VKWHM@users.noreply.github.com>
2025-04-14 09:14:16 +0000

    enhencement in fzf

--- nix/fzf.nix
+++ nix/fzf.nix
@@ -2,7 +2,8 @@
 let
   themeIs = light: dark: "\\$([[ \\\"\\$WHM_APPEARANCE\\\" == \\\"Light\\\"* ]] && echo ${light} || echo ${dark})";
   ctMocha = "bg:#24273A,bg+:#1E2030,fg:#D9E0EE,fg+:#A5ADCB,hl:#89B4FA,hl+:#89B4FA,info:#F9E2AF,marker:#D9E0EE,pointer:#D9E0EE,prompt:#F9E2AF,spinner:#F9E2AF";
-  fileOrDir = "if [ -d {} ]; then eza --tree --color=always {} | head -300; else bat --theme=\\\"${themeIs "Catppuccin Latte" "Catppuccin Mocha"}\\\" -n --color=always --line-range :500 {}; fi";
+  ctLatte = "bg:#EFF1F5,bg+:#E6E9EF,fg:#4C4F69,fg+:#5C5F77,hl:#1E66F5,hl+:#1E66F5,info:#DC8A78,marker:#4C4F69,pointer:#4C4F69,prompt:#DC8A78,spinner:#DC8A78";
+  fileOrDir = "if [ -d {} ]; then eza --tree --color=always {} | head -300; else ${pkgs.bat}/bin/bat --theme=\\\"${themeIs "Catppuccin Latte" "Catppuccin Mocha"}\\\" -n --color=always --line-range :500 {}; fi";
   defaultCmd = "fd --hidden --strip-cwd-prefix --exclude .git";
 in {
   enable = true;
@@ -11,6 +12,7 @@ in {
     "--height=80%"
     "--layout=reverse"
     "--border"
+    "--tmux"
     "--color ${ctMocha}"
   ];
   defaultCommand = defaultCmd;
