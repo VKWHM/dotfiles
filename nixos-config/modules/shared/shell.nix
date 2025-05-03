465285db00e9c68b9ed710c6f1d02d5fb2d047ac
VKWHM <alasaal3@gmail.com>
2025-05-03 17:04:42 +0300

    Adding tmux search keybindings to shell.nix

--- nixos-config/modules/shared/shell.nix
+++ nixos-config/modules/shared/shell.nix
@@ -40,4 +40,57 @@ in {
     btop
     go cargo # for neovim
   ];
+  # Tmux search keybindings
+  programs.tmux.searchKeys = [
+    {
+      name = "URL";
+      key = "C-u";
+      regex = "(https?://|git@|git://|ssh://|ftp://|file:///)[[:alnum:]?=%/_.:,;~@!#$&()*+-]*";
+    }
+    {
+      name = "Number";
+      key = "C-d";
+      regex = "[[:digit:]]+";
+    }
+    {
+      name = "File";
+      key = "C-f";
+      regex = "(^|^\\.|[[:space:]]|[[:space:]]\\.|[[:space:]]\\.\\.|^\\.\\.)[[:alnum:]~_-]*/[][[:alnum:]_.#$%&+=/@-]*";
+    }
+    {
+      name = "Base64";
+      key = "M-b";
+      regex = "([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?";
+    }
+    {
+      name = "UUID";
+      key = "M-u";
+      regex = "[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}";
+    }
+    {
+      name = "Docker ID";
+      key = "M-d";
+      regex = "[0-9a-f]{12}";
+    }
+    {
+      name = "MAC Address";
+      key = "M-m";
+      regex = "([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})";
+    }
+    {
+      name = "Hash/SHA";
+      key = "M-h";
+      regex = "\\b([0-9a-f]{7,40}|[[:alnum:]]{52}|[0-9a-f]{64})\\b";
+    }
+    {
+      name = "IP Address";
+      key = "M-i";
+      regex = "[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}";
+    }
+    {
+      name = "Email Address";
+      key = "M-e";
+      regex = "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}\\b";
+    }
+  ];
 }
