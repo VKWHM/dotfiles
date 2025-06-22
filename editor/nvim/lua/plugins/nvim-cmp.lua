1c04ca39f52add1eec35730c9448007cbaa91157
VKWHM <111775089+VKWHM@users.noreply.github.com>
2025-06-22 20:44:23 +0300

    feat: enhance Copilot integration and configuration
    Revised Copilot plugin setup to include new configurations and
    dependencies. Added support for Copilot suggestions and status
    integration with lualine. Adjusted autocompletion sources to
    prioritize Copilot and snippets for improved developer experience.

--- editor/nvim/lua/plugins/nvim-cmp.lua
+++ editor/nvim/lua/plugins/nvim-cmp.lua
@@ -112,11 +112,11 @@ return {
 			},
 			-- sources for autocompletion
 			sources = cmp.config.sources({
-				{ name = "copilot" },
 				{ name = "lazydev" },
 				{ name = "nvim_lsp" },
-				{ name = "luasnip" }, -- snippets
 				{ name = "path" }, -- file system paths
+				{ name = "copilot" },
+				{ name = "luasnip" }, -- snippets
 				{ name = "buffer" }, -- text within current buffer
 			}),
 			experimental = {
