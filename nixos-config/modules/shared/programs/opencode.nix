7e1b8225985ef4543368b3024d83701cf61348b9
VKWHM <111775089+VKWHM@users.noreply.github.com>
2026-05-24 20:25:47 +0300

    feat(opencode): configure git and github MCP servers

--- nixos-config/modules/shared/programs/opencode.nix
+++ nixos-config/modules/shared/programs/opencode.nix
@@ -1,9 +1,9 @@
-{
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   config.programs.opencode = {
     enable = true;
     package = pkgs.hello;
   };
+  config.home.packages = with pkgs; [
+    mcp-server-git
+  ];
 }
